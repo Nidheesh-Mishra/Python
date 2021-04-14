eb570008a288db8a7380de540a3af9f225794e17
Nidheesh-Mishra <80092648+Nidheesh-Mishra@users.noreply.github.com>
2021-04-14 12:16:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+rad = float(input("Enter the radius of the circle : "))
+print("The area of the circle with radius",rad,"is: ", ((22/7)*rad*rad))
+filen = input("Input the Filename: ")
+f= filen.split(".")
+print ("The extension of the file is : "+ f[-1])
